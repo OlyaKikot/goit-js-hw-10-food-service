5fed637d90ca84ecc3bd8551dd609a0e469fbe98
Olya Kikot <kikotolya1989@gmail.com>
2021-07-26 10:36:36 +0300

    corrections

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './sass/main.scss';
-import dish from '../src/templates/dish.hbs';
+import createDishMarkup from '../src/templates/dish.hbs';
 import menu from './menu.json';
 
-console.log(dish(menu[0]));
+
 const refs = {
   switch: document.querySelector('#theme-switch-toggle'),
   body: document.querySelector('body'),
@@ -18,26 +18,23 @@ refs.switch.addEventListener('change', onSwitch);
 document.addEventListener('DOMContentLoaded', onDocumentLoad);
 
 function onSwitch() {
-  if (refs.body.classList.contains(Theme.DARK)) {
-    refs.body.classList.remove(Theme.DARK);
-    refs.body.classList.add(Theme.LIGHT);
+  
+  if (refs.body.classList.replace(Theme.DARK, Theme.LIGHT)) {
+ 
     localStorage.setItem('isDarkTheme', false);
     return;
   }
-  refs.body.classList.remove(Theme.LIGHT);
-  refs.body.classList.add(Theme.DARK);
+  refs.body.classList.replace(Theme.LIGHT, Theme.DARK)
   localStorage.setItem('isDarkTheme', true);
 }
 
 function onDocumentLoad() {
+  
   if (localStorage.getItem('isDarkTheme') === 'true') {
     refs.body.classList.add(Theme.DARK);
     refs.switch.defaultChecked = true;
   }
-}
-
-function createDishMarkup(menu) {
-  return menu.map(dish).join('');
+  else{refs.body.classList.add(Theme.LIGHT)}
 }
 
 refs.listMenu.innerHTML = createDishMarkup(menu);
